c304b53d158e5cdc347328398b6888dc868a9c09
Anshu Sinha <anshujuly2@gmail.com>
2023-07-25 08:57:06 +0530

    Add a function to find the sum of numbers in an array using recurisve function

--- Algorithms/quicksort.py
+++ Algorithms/quicksort.py
@@ -5,6 +5,15 @@ def arr_sum(arr):
         sum += num
     return sum
 
+# solving the arr_sum() using divide and conquer and applying recursive function
+def rec_sum(arr):
+    sum = 0
+    if not arr:
+        return 0
+    else:
+        return arr[0] + rec_sum(arr[1:])
+
+
 # ----- MAIN ----- #
 if __name__ == '__main__':
 
@@ -13,4 +22,5 @@ if __name__ == '__main__':
     # # arr_sum()
     # print(arr_sum([1, 2, 3, 4, 5]))
 
-    
+    # # rec_sum()
+    # print(rec_sum([1, 2, 3, 4, 5]))
