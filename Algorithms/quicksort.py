1af50cc1efa212bccfb4554301a63f160edecea1
Anshu Sinha <anshujuly2@gmail.com>
2023-07-25 08:38:35 +0530

    Add a function to find the sum of numbers in an array

---
+++ Algorithms/quicksort.py
@@ -0,0 +1,13 @@
+# simple prograam to find sum of numbers in an array
+def arr_sum(arr):
+    sum = 0
+    for num in arr:
+        sum += num
+    return sum
+
+# ----- MAIN ----- #
+if __name__ == '__main__':
+
+    """ Uncomment to run specific block of code below 👇🏻"""
+
+    print(arr_sum([1, 2, 3, 4, 5]))
