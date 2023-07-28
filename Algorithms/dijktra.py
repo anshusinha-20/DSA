85d43b340c7ad094c2076e4a6e43ff3196b82683
Anshu Sinha <anshujuly2@gmail.com>
2023-07-28 20:17:53 +0530

    Add dijktra's algorithm

---
+++ Algorithms/dijktra.py
@@ -0,0 +1,61 @@
+'''
+Djikstra's algorithm
+'''
+# ----- Graph table ----- #
+# start node
+graph = {}
+graph['start'] = {}
+graph['start']['A'], graph['start']['B'] = 6, 2
+# print(graph['start'].values())
+# print(graph['start']['A'])
+
+# A and B node
+graph['A'] = {}
+graph['B'] = {}
+graph['A']['fin'], graph['B']['A'], graph['B']['fin'] = 1, 3, 5
+# print(graph)
+
+# finish node
+graph['finish'] = {}
+# print(graph)
+
+# ----- Costs table ----- #
+costs = {}
+infinity = float('inf')
+costs['A'], costs['B'], costs['fin'] = 6, 2, infinity
+# print(costs)
+
+# ----- Parents table ----- #
+parents = {}
+parents['A'], parents['B'], parents['fin'] = 'start', 'start', None
+# print(parents)
+
+# Array to keep track of the processed nodes
+processed_nodes = []
+
+# function to find the lowest cost node
+def find_lowest_cost_node(costs):
+    lowest_cost = float('inf')
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed_nodes:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+# print(find_lowest_cost_node(costs))
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neigbours = graph[node]
+    for neighbour in neigbours:
+        new_cost = cost + costs[neighbour]
+        if new_cost < costs[neighbour]:
+            costs[neighbour] = new_cost
+            parents[neighbour] = node
+    processed_nodes.append(node)
+    node = find_lowest_cost_node(costs)
+
+print(processed_nodes)
