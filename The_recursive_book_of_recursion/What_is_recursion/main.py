390fdffe34417696d60012a1c6b8318c54cefcc4
Anshu Sinha <anshujuly2@gmail.com>
2023-08-16 11:28:50 +0530

    add print statements

--- The_recursive_book_of_recursion/What_is_recursion/main.py
+++ The_recursive_book_of_recursion/What_is_recursion/main.py
@@ -1,11 +1,16 @@
 # FUNCTION CALL
 def a():
+    print('a() was called')
     b()
+    print('a() is returning')
 
 def b():
+    print('b() was called')
     c()
+    print('b() is returning')
 
 def c():
-    return None
-
+    print('c() was called')
+    print('c() is returning')
+    
 a()
