f0b3a37f7200c65ec06838e8fe203fcc8cdaab40
Anshu Sinha <anshujuly2@gmail.com>
2023-08-16 11:39:37 +0530

    Add a code on working of stacks

--- The_recursive_book_of_recursion/What_is_recursion/main.py
+++ The_recursive_book_of_recursion/What_is_recursion/main.py
@@ -1,16 +1,27 @@
-# FUNCTION CALL
-def a():
-    print('a() was called')
-    b()
-    print('a() is returning')
+# # FUNCTION CALL
+# def a():
+#     print('a() was called')
+#     b()
+#     print('a() is returning')
+#
+# def b():
+#     print('b() was called')
+#     c()
+#     print('b() is returning')
+#
+# def c():
+#     print('c() was called')
+#     print('c() is returning')
+#
+# a()
 
-def b():
-    print('b() was called')
-    c()
-    print('b() is returning')
+# ----- #
 
-def c():
-    print('c() was called')
-    print('c() is returning')
-    
-a()
+# STACK
+num_stack = []
+num_stack.append('5')
+print(','.join(num_stack))
+num_stack.append('3')
+print(','.join(num_stack))
+num_stack.append('4')
+print(','.join(num_stack))
