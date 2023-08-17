872491e40345bde2982309c7548c9a5aa2b6842b
Anshu Sinha <anshujuly2@gmail.com>
2023-08-17 11:52:26 +0530

    Add a code to calculate exponents

--- The_recursive_book_of_recursion/Recursion_vs_iteration/main.py
+++ The_recursive_book_of_recursion/Recursion_vs_iteration/main.py
@@ -50,20 +50,51 @@
 
 # ----- #
 
-# CONVERTING AN ITERATIVE ALGORITHM INTO A RECURSIVE ALGORITHM
-num = 5
-while num > 0:
-    print('Hello world!')
-    num -= 1
+# # CONVERTING AN ITERATIVE ALGORITHM INTO A RECURSIVE ALGORITHM
+# num = 5
+# while num > 0:
+#     print('Hello world!')
+#     num -= 1
+#
+# print('\n')
+#
+# def greet(num):
+#     print('Hello world!')
+#     num -= 1
+#     if num < 1:
+#         return None
+#     else:
+#         greet(num)
+#
+# greet(5)
+
+# ----- #
+
+# CASE STUDY: CALCULATING EXPONENTS
+def exponent_by_iteration(num, pow):
+    prod = 1
+    for i in range(pow):
+        prod *= num
+    return prod
+
+print(exponent_by_iteration(3, 6))
+print(exponent_by_iteration(10, 3))
+print(exponent_by_iteration(17, 10))
 
 print('\n')
 
-def greet(num):
-    print('Hello world!')
-    num -= 1
-    if num < 1:
-        return None
-    else:
-        greet(num)
+def exponent_by_recursion(num, pow):
+    if pow == 0:
+        return 1
+    elif pow == 1:
+        return num
+    elif pow % 2 == 0:  # even power
+        result = exponent_by_iteration(num, pow // 2)
+        return result * result
+    elif pow % 2 != 0:  # odd power
+        result = exponent_by_iteration(num, pow // 2)
+        return result * result * num
 
-greet(5)
+print(exponent_by_recursion(3, 6))
+print(exponent_by_recursion(10, 3))
+print(exponent_by_recursion(17, 10))
