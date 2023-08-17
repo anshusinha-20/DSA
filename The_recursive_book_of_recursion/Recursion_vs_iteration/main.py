511660987b2558adf6576399ec6d8c399342869d
Anshu Sinha <anshujuly2@gmail.com>
2023-08-17 10:08:24 +0530

    Add a code on recursive approach to find factorial of a number

--- The_recursive_book_of_recursion/Recursion_vs_iteration/main.py
+++ The_recursive_book_of_recursion/Recursion_vs_iteration/main.py
@@ -1,8 +1,19 @@
-# THE ITERATIVE FACTORIAL ALGORITHM
-def factorial(num):
-    prod = 1
-    for i in range(1, num + 1):
-        prod *= i
-    return prod
+# # THE ITERATIVE FACTORIAL ALGORITHM
+# def factorial(num):
+#     prod = 1
+#     for i in range(1, num + 1):
+#         prod *= i
+#     return prod
+#
+# print(factorial(2))
 
-print(factorial(2))
+# ----- #
+
+# # THE RECURSIVE FACTORIAL ALGORITHM
+# def factorial(num):
+#     if num == 0:
+#         return 1
+#     else:
+#         return num * factorial(num - 1)
+#
+# print(factorial(2))
