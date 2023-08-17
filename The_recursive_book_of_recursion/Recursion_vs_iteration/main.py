56e255de52cc7b3c5b6ddb3b31d246a4b425955d
Anshu Sinha <anshujuly2@gmail.com>
2023-08-17 10:24:46 +0530

    Add a code on iterative way to find the fibonacci series

--- The_recursive_book_of_recursion/Recursion_vs_iteration/main.py
+++ The_recursive_book_of_recursion/Recursion_vs_iteration/main.py
@@ -11,9 +11,21 @@
 
 # # THE RECURSIVE FACTORIAL ALGORITHM
 # def factorial(num):
-#     if num == 0:
+#     if num == 1:
 #         return 1
 #     else:
 #         return num * factorial(num - 1)
 #
 # print(factorial(2))
+
+# ----- #
+
+# THE ITERATIVE FIBONACCI ALGORITHM
+def fibonacci(nth_num):
+    a, b = 1, 1
+    for i in range(1, nth_num):
+        a, b = b, a + b
+        print(f'a = {a}, b = {b}')
+    return b
+
+print(fibonacci(10))
