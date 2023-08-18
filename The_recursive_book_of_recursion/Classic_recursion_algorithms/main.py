bdc779ec2e4617b3bc67a5efd4c3a039461834c4
Anshu Sinha <anshujuly2@gmail.com>
2023-08-18 18:02:17 +0530

    Add a code on tower of hanoi

--- The_recursive_book_of_recursion/Classic_recursion_algorithms/main.py
+++ The_recursive_book_of_recursion/Classic_recursion_algorithms/main.py
@@ -27,15 +27,48 @@
 
 # ----- #
 
-# DETECTING PALINDROME
-def check_palindrome(string):
-    if len(string) == 0 or len(string) == 1:
-        return True
+# # DETECTING PALINDROME
+# def check_palindrome(string):
+#     if len(string) == 0 or len(string) == 1:
+#         return True
+#     else:
+#         head = string[0]
+#         middle = string[1:-1]
+#         last = string[-1]
+#         return head == last and check_palindrome(middle)
+#
+#
+# print(check_palindrome('thisiswherepytonnotyperehwsisiht'))
+
+# ----- #
+
+# SOLVING THE TOWER OF HANOI
+total_disks = int(input('Enter total number of disks: '))
+
+TOWERS = {
+    'A': list(reversed(range(1, total_disks + 1))),
+    'B': [],
+    'C': []
+}
+
+# print(TOWERS)
+
+def move_one_disk(start_tower, end_tower):
+    disk = TOWERS[start_tower].pop()
+    TOWERS[end_tower].append(disk)
+
+def solve_tower_of_hanoi(number_of_disks, start_tower, end_tower, temp_tower):
+    if number_of_disks == 1:
+        move_one_disk(start_tower, end_tower)
+        print(TOWERS)
+        return
     else:
-        head = string[0]
-        middle = string[1:-1]
-        last = string[-1]
-        return head == last and check_palindrome(middle)
+        solve_tower_of_hanoi(number_of_disks - 1, start_tower, temp_tower, end_tower)
+        move_one_disk(start_tower, end_tower)
+        print(TOWERS)
+        solve_tower_of_hanoi(number_of_disks - 1, temp_tower, end_tower, start_tower)
+        print(TOWERS)
+        return
 
 
-print(check_palindrome('thisiswherepytonnotyperehwsisiht'))
+solve_tower_of_hanoi(total_disks, 'A', 'C', 'B')
