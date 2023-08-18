5fc351e71f73e134aa6e60904f3a7d9ac931e401
Anshu Sinha <anshujuly2@gmail.com>
2023-08-18 10:40:55 +0530

    Add a code to reverse the string recursively

--- The_recursive_book_of_recursion/Classic_recursion_algorithms/main.py
+++ The_recursive_book_of_recursion/Classic_recursion_algorithms/main.py
@@ -1,12 +1,26 @@
-# SUMMING NUMBERS IN AN ARRAY
-def sum_arr(arr):
-    if len(arr) == 0:
-        return 0
+# # SUMMING NUMBERS IN AN ARRAY
+# def sum_arr(arr):
+#     if len(arr) == 0:
+#         return 0
+#     else:
+#         head = arr[0]
+#         tail = arr[1:]
+#         return head + sum_arr(tail)
+#
+#
+# num_arr = [1, 2, 3, 4, 5]
+# print(sum_arr(num_arr))
+
+# ----- #
+
+# REVERSING A STRING
+def rev_str(string):
+    if len(string) == 0 or len(string) == 1:
+        return string
     else:
-        head = arr[0]
-        tail = arr[1:]
-        return head + sum_arr(tail)
+        head = string[0]
+        tail = string[1:]
+        return rev_str(tail) + head
 
 
-num_arr = [1, 2, 3, 4, 5]
-print(sum_arr(num_arr))
+print(rev_str('hello'))
