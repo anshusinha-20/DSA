bbbebfc9a94acabc49a6bea52d5aee9f2465cf8a
Anshu Sinha <anshujuly2@gmail.com>
2023-08-22 20:26:46 +0530

    Add a code on recursive approach for quicksort

--- The_recursive_book_of_recursion/Divide_and_conquer_algorithms/main.py
+++ The_recursive_book_of_recursion/Divide_and_conquer_algorithms/main.py
@@ -1,20 +1,38 @@
-# BINARY SEARCH: FINDING A BOOK IN AN ALPHABETIZED BOOKSHELF
-def binary_search(number, arr, left=None, right=None):
-    if left == None:
-        left = 0
-    if right == None:
-        right = len(arr)
+# # BINARY SEARCH: FINDING A BOOK IN AN ALPHABETIZED BOOKSHELF
+# def binary_search(number, arr, left=None, right=None):
+#     if left == None:
+#         left = 0
+#     if right == None:
+#         right = len(arr) - 1
+#
+#     print(f'Searching: {arr[left:right + 1]}')
+#
+#     mid = (left + right) // 2
+#
+#     if number == arr[mid]:
+#         return mid
+#     elif number < arr[mid]:
+#         return binary_search(number, arr, left, mid - 1)
+#     elif number > arr[mid]:
+#         return binary_search(number, arr, mid + 1, right)
+#
+#
+# print(binary_search(11, [1, 4, 8, 11, 13, 16, 19, 19]))
 
-    print(f'Searching: {arr[left:right]}')
+# ----- #
 
-    mid = (left + right) // 2
+# QUICKSORT: SPLITTING AN UNSORTED PILE OF BOOKS INTO SORTED PILES
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
 
-    if number == arr[mid]:
-        return mid
-    elif number < arr[mid]:
-        return binary_search(number, arr, left, mid - 1)
-    elif number > arr[mid]:
-        return binary_search(number, arr, mid + 1, right)
+    pivot = arr[len(arr) // 2]
+    left = [i for i in arr if i < pivot]
+    middle = [i for i in arr if i == pivot]
+    right = [i for i in arr if i > pivot]
+
+    return quicksort(left) + middle + quicksort(right)
 
 
-print(binary_search(11, [1, 4, 8, 11, 13, 16, 19, 19]))
+array = [0, 7, 6, 3, 1, 2, 5, 4]
+print(quicksort(array))
