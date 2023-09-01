54c241dde477b310d0e302ef559776e2f4ad8f76
Anshu Sinha <anshujuly2@gmail.com>
2023-09-01 17:42:15 +0530

    Add a code on pass by value and pass by reference in respect to functions in cpp

--- A2Z_DSA_C++/functions.cpp
+++ A2Z_DSA_C++/functions.cpp
@@ -34,20 +34,61 @@
 // }
 
 // ----- //
-// FUNCTION TO RETURN THE MAX OF TWO NUMBERS
+
+// // FUNCTION TO RETURN THE MAX OF TWO NUMBERS
+// #include<iostream>
+// using namespace std;
+
+// int max(int num1, int num2) {
+//     if (num1 > num2) {
+//         return num1;
+//     }
+//     return num2;
+// }
+
+// int main() {
+//     int num1, num2;
+//     cout << "Enter two numbers: ";
+//     cin >> num1 >> num2;
+//     cout << "The greatest between " << num1 << " and " << num2 << " is: " << max(num1, num2) << endl;
+// }
+
+// ----- //
+
+// // PASS BY VALUE
+// #include<iostream>
+// using namespace std;
+
+// void random_stuff(int num) {
+//     cout << num << endl;
+//     num += num;
+//     cout << num << endl;
+// }
+
+// int main() {
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+//     random_stuff(num);  // sends a copy of the original num to the random_stuff function
+//     cout << num << endl;  // will print 5
+
+//     return 0;
+// }
+
+// PASS BY REFERENCE
 #include<iostream>
 using namespace std;
 
-int max(int num1, int num2) {
-    if (num1 > num2) {
-        return num1;
-    }
-    return num2;
+void random_stuff2(int &num) {  // got the original address of the 'num' value
+    num += 5;
 }
 
 int main() {
-    int num1, num2;
-    cout << "Enter two numbers: ";
-    cin >> num1 >> num2;
-    cout << "The greatest between " << num1 << " and " << num2 << " is: " << max(num1, num2) << endl;
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    random_stuff2(num);
+    cout << num << endl;  // modified value of num will be printed
+
+    return 0;
 }
