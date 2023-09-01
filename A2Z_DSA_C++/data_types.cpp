6e39d4c5dc1c0c043ec84c6abc4c5d3f10dbfaeb
Anshu Sinha <anshujuly2@gmail.com>
2023-09-01 10:59:13 +0530

    Add code on data types in cpp

--- A2Z_DSA_C++/data_types.cpp
+++ A2Z_DSA_C++/data_types.cpp
@@ -2,7 +2,54 @@
 using namespace std;
 
 int main() {
-    
+    // integer data type
+    int x;
+    cout << "Enter value of x: ";
+    cin >> x;
+
+    long x2;
+    cout << "Enter value of x2: ";
+    cin >> x2;
+
+    long long x3;
+    cout << "Enter value of x3: ";
+    cin >> x3;
+
+    // ----- //
+
+    // decimal data type
+    float y;
+    cout << "Enter value of y: ";
+    cin >> y;
+
+    double y2;
+    cout << "Enter value of y2: ";
+    cin >> y2;
+
+    // ----- //
+
+    // string data type
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
+
+    string s2, s3;
+    cout << "Enter two strings separeted by a space: ";
+    cin >> s2 >> s3;
+    cout << s2 << " " << s3 << endl;
+
+    string s4;
+    cout << "Enter a string: ";
+    getline(cin, s4);  // getting entire line of string
+    cout << s4 << endl;
+
+    // ----- //
+
+    // character datatype for single string input
+    char c;
+    cout << "Enter a character: ";
+    cin >> c;
+    cout << c << endl;
 
     return 0;
 }
