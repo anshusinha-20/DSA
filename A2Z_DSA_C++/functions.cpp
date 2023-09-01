fb4ae595f432d973386f3ca571519864376a8b63
Anshu Sinha <anshujuly2@gmail.com>
2023-09-01 17:28:29 +0530

    Add a code to find the maximum between two numbers

--- A2Z_DSA_C++/functions.cpp
+++ A2Z_DSA_C++/functions.cpp
@@ -16,19 +16,38 @@
 
 // ----- //
 
-// PARAMETERISED RETURN FUNCTION
+// // PARAMETERISED RETURN FUNCTION
+// #include<iostream>
+// using namespace std;
+
+// int sum(int num1, int num2) {
+//     return num1 + num2;
+// }
+
+// int main() {
+//     int num1, num2;
+//     cout << "Enter two integers: ";
+//     cin >> num1 >> num2;
+//     cout << "Sum of " << num1 << " and " << num2 << " is: " << sum(num1, num2) << endl;
+
+//     return 0;
+// }
+
+// ----- //
+// FUNCTION TO RETURN THE MAX OF TWO NUMBERS
 #include<iostream>
 using namespace std;
 
-int sum(int num1, int num2) {
-    return num1 + num2;
+int max(int num1, int num2) {
+    if (num1 > num2) {
+        return num1;
+    }
+    return num2;
 }
 
 int main() {
     int num1, num2;
-    cout << "Enter two integers: ";
+    cout << "Enter two numbers: ";
     cin >> num1 >> num2;
-    cout << "Sum of " << num1 << " and " << num2 << " is: " << sum(num1, num2) << endl;
-
-    return 0;
+    cout << "The greatest between " << num1 << " and " << num2 << " is: " << max(num1, num2) << endl;
 }
