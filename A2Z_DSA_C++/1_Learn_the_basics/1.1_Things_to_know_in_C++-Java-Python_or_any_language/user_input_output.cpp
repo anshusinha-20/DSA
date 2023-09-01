c07084d7821b790e5d39bbfb9a01cd45e52aecb7
Anshu Sinha <anshujuly2@gmail.com>
2023-09-01 10:16:41 +0530

    Add code for basic input output command in c++

---
+++ A2Z_DSA_C++/1_Learn_the_basics/1.1_Things_to_know_in_C++-Java-Python_or_any_language/user_input_output.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+
+    cout << "Hello world!" << endl;
+    cout << "Enter the value of x and y: ";
+    cin >> x >> y;
+    cout << "The vlaue of x is: " << x << " and the value of y is: " << y << endl;
+
+    return 0;
+}
