2999ecdbc84c816e0ac5380204dbbf242ac58b16
Anshu Sinha <anshujuly2@gmail.com>
2023-09-01 16:12:45 +0530

    Add a code on strings in cpp

---
+++ A2Z_DSA_C++/arrays_and_strings.cpp
@@ -0,0 +1,45 @@
+// // 1D ARRAY
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int arr[5];
+
+//     cout << "Enter 5 array elements: ";
+//     cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+//     cout << arr[0] << endl;
+
+//     return 0;
+// }
+
+// ----- //
+
+// // 2D ARRAY
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int arr2D[3][5];  // array with 3 rows and 5 columns
+
+//     arr2D[1][2] = 32;
+//     cout << arr2D[1][2] << endl;
+//     cout << arr2D[0][1] << endl;  // garbage value
+//     cout << arr2D[1][3] << endl;  // garbage value
+
+//     return 0;
+// }
+
+// ----- //
+
+// STRING
+#include<iostream>
+using namespace std;
+
+int main() {
+    string s = "Anshu";
+    cout << s.length() << endl;
+    cout << s[0] << endl;
+    cout << s[s.length() - 1] << endl;
+    
+    return 0;
+}
