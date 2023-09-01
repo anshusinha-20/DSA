d4efb818ec17152b6a5862ce4beceedc13f9fdfa
Anshu Sinha <anshujuly2@gmail.com>
2023-09-01 14:25:26 +0530

    Add a code on 1d array

---
+++ A2Z_DSA_C++/arrays.cpp
@@ -0,0 +1,13 @@
+// 1D ARRAY
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+
+    cout << "Enter 5 array elements: ";
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+    cout << arr[0] << endl;
+
+    return 0;
+}
