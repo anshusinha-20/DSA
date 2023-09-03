fbf97e9ee8bc0a2cf7f0bbda645a3401806c59d2
Anshu Sinha <anshujuly2@gmail.com>
2023-09-03 11:05:27 +0530

    Add a cpp file on lists

---
+++ A2Z_DSA_C++/1_Learn_the_basics/1.3_Learn_STL/list.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+void lists() {
+    list<int> l1;
+    l1.emplace_back(2); // {2}
+    l1.emplace_front(3); // {3, 2}
+
+    for (auto i : l1) {
+        cout << i << " "; // 3 2
+    }
+    cout << endl;
+}
+
+int main() {
+    lists();
+
+    return 0;
+}
