09ec6abdcd2372267ce10cc31bba1ed179efd3ae
Anshu Sinha <anshujuly2@gmail.com>
2023-09-03 20:24:19 +0530

    Add a cpp code on multimaps

---
+++ A2Z_DSA_C++/1_Learn_the_basics/1.3_Learn_STL/multimap.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+void multimaps() { // stores elements in key value order, in sorted order but the keys can be duplicate
+    multimap<int, int> mmp;
+    // rest all functions are similar to map
+}
+
+int main() {
+    multimaps();
+
+    return 0;
+}
