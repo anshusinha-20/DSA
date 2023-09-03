3be774665a53d557084a89217ba271685691b5bd
Anshu Sinha <anshujuly2@gmail.com>
2023-09-03 09:26:23 +0530

    Add a code on pair in cpp

---
+++ A2Z_DSA_C++/1_Learn_the_basics/1.3_Learn_STL/pair.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<utility>
+using namespace std;
+
+void pairs() {
+    pair <int, int> p1 = {1, 2};
+    cout << p1.first << " " << p1.second << endl;
+
+    pair<int, pair<int, int>> p2 = {1, {2, 3}};
+    cout << p2.first << " " << p2.second.first << " " << p2.second.second << endl;
+
+    pair<int, int> arr[] = {{1, 2}, {3, 4}, {5, 6}};
+    cout << arr[1].second << endl;
+}
+
+int main() {
+    pairs();
+
+    return 0;
+}
