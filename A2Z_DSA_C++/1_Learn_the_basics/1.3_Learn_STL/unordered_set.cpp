b0d4a5476f3382bad26ba78a8a1b352f04820c6f
Anshu Sinha <anshujuly2@gmail.com>
2023-09-03 19:55:28 +0530

    Add a cpp file on unordered sets

---
+++ A2Z_DSA_C++/1_Learn_the_basics/1.3_Learn_STL/unordered_set.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+
+void unordered_sets() {
+    unordered_set<int> us;
+
+    // the only difference is that it stores elements in unsorted order
+    // rest all functions are similar to set
+}
+
+int main() {
+    unordered_sets();
+
+    return 0;
+}
