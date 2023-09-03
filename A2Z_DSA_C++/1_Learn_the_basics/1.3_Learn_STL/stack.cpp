ff0a1573d1a8f66f5801f72aef30bc87fe25472d
Anshu Sinha <anshujuly2@gmail.com>
2023-09-03 11:24:54 +0530

    Add a cpp file on stacks

---
+++ A2Z_DSA_C++/1_Learn_the_basics/1.3_Learn_STL/stack.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void stacks() { // stacks work on LIFO principle i.e, the element was pushed the last is the element which will be popped out first
+    stack<int> st;
+    st.push(1); 
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.emplace(5);
+
+    cout << st.top() << endl; // prints the last/top element 
+    st.pop(); // last/top element is popped out
+    cout << st.top() << endl;
+
+    cout << st.size() << endl; // prints the size of the stack
+    cout << st.empty() << endl; // prints if the stack is empty or not i.e, 0 - False and 1 - True
+}
+
+int main() {
+    stacks();
+
+    return 0;
+}
