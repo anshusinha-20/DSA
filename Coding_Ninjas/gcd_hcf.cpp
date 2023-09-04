8a404f3a25ff6d6b5cadd7c09e2b7ac2579c7bd2
Anshu Sinha <anshujuly2@gmail.com>
2023-09-04 14:51:50 +0530

    Add best approach code to gcd

--- Coding_Ninjas/gcd_hcf.cpp
+++ Coding_Ninjas/gcd_hcf.cpp
@@ -10,3 +10,20 @@ int calcGCD(int n, int m){
 }
 
 // BEST APPROACH - EUCLIDEAN ALGORIHTM
+int calcGCD(int n, int m){
+    // Write your code here.
+    while (n > 0 && m > 0) {
+        if (n > m) {
+            n = n % m;
+        }
+        else {
+            m = m % n;
+        }
+    }
+    if (n == 0) {
+        return m;
+    }
+    else {
+        return n;
+    }
+}
