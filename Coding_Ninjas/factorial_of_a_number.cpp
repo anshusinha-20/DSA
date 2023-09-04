28453be6d33768b79ece12a9e8c763f9fc6d49cc
Anshu Sinha <anshujuly2@gmail.com>
2023-09-04 19:14:47 +0530

    Add a code to find factorial of a number iteratively

--- Coding_Ninjas/factorial_of_a_number.cpp
+++ Coding_Ninjas/factorial_of_a_number.cpp
@@ -17,3 +17,31 @@ int main() {
 
     return 0;
 }
+
+// ITERATIVE APPROACH
+#include<iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    int n;
+    cin >> n;
+    int prod = 1;
+
+    if (n > 0) {
+        for (int i = n; i > 0; i -= 1) {
+            prod *= i;
+        }
+        cout << prod;
+    }
+
+    else if (n == 0) {
+        cout << 1;
+    }
+
+    else {
+        cout << "Error";
+    }
+
+    return 0;
+}
