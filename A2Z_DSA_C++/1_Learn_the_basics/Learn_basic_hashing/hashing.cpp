94ec7762e288e63b7d005b9b149504bc5464cad3
Anshu Sinha <anshujuly2@gmail.com>
2023-09-05 16:53:23 +0530

    Add a code to found frequency of an element in a range

--- A2Z_DSA_C++/1_Learn_the_basics/Learn_basic_hashing/hashing.cpp
+++ A2Z_DSA_C++/1_Learn_the_basics/Learn_basic_hashing/hashing.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-// // BRUTE FORCE APPROACH TO FIND THE NUMBER OF OCCURENCES OF AN ELEMENT IN A VECTOR - Big theta (n)
+// // BRUTE FORCE APPROACH TO FIND THE NUMBER OF OCCURENCES OF AN INTEGER IN A VECTOR - Big theta (n)
 // int main() {
 //     vector<int> v1 = {1, 2, 3, 1, 2, 3, 1, 1, 1};
 //     int count = 0;
@@ -21,37 +21,54 @@ using namespace std;
 //     return 0;
 // }
 
-// HASING APPROACH
-int hashh[10000000]; // can take 10^7 values, and everything is initialized as zero
+// // HAShING APPROACH TO FIND THE NUMBER OF OCCURENCES OF AN INTEGER
+// int hashh[10000000]; // can take 10^7 values, and everything is initialized as zero
+// int main() {
+//     int arr_size; // size of the array
+//     cout << "Enter size of the array: ";
+//     cin >> arr_size;
+
+//     int arr[arr_size]; // initializing the size of the array
+
+//     cout << "Enter array elements: ";
+//     for (int i = 0; i < arr_size; i += 1) {   
+//         cin >> arr[i]; // taking the inputs and storing it in the array
+//     }
+
+//     // precomputation
+//     // int hashh[1000000] = {0}; // hash[10000000] will be segmentation fault, i.e, 10^7, max limit 10^6
+//     for (int i = 0; i < arr_size; i += 1) {
+//         hashh[arr[i]] += 1; // loop through the array elements and increment the value of hash elements, i.e, if arr[0] = 1, arr[1] = 1, than hash[1] += 1, hash[1] += 1, so 1 exited two times, was stored in the hash array
+//     }
+
+//     // fetching 
+//     int num_el;
+//     cout << "Enter number of elements to search for: ";
+//     cin >> num_el;
+    
+//     for (int i = 0; i < num_el; i += 1) {
+//         int el;
+//         cout << "Enter the element: ";
+//         cin >> el;
+//         cout << hashh[el] << endl;
+//     }
+
+//     return 0;
+// }
+
+// HAShING APPROACH TO FIND THE NUMBER OF OCCURENCES OF AN INTEGER
 int main() {
-    int arr_size; // size of the array
-    cout << "Enter size of the array: ";
-    cin >> arr_size;
-
-    int arr[arr_size]; // initializing the size of the array
-
-    cout << "Enter array elements: ";
-    for (int i = 0; i < arr_size; i += 1) {   
-        cin >> arr[i]; // taking the inputs and storing it in the array
-    }
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
 
     // precomputation
-    // int hashh[1000000] = {0}; // hash[10000000] will be segmentation fault, i.e, 10^7, max limit 10^6
-    for (int i = 0; i < arr_size; i += 1) {
-        hashh[arr[i]] += 1; // loop through the array elements and increment the value of hash elements, i.e, if arr[0] = 1, arr[1] = 1, than hash[1] += 1, hash[1] += 1, so 1 exited two times, was stored in the hash array
+    int hashh[256] = {0};
+    for (int i = 0; i < s.size(); i += 1) {
+        hashh[s[i]] += 1; 
     }
 
-    // fetching 
-    int num_el;
-    cout << "Enter number of elements to search for: ";
-    cin >> num_el;
-    
-    for (int i = 0; i < num_el; i += 1) {
-        int el;
-        cout << "Enter the element: ";
-        cin >> el;
-        cout << hashh[el] << endl;
-    }
-
-    return 0;
+    // fetching
+    int n;
+    cout << "Enter number of characters to search for: "
 }
