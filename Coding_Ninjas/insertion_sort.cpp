fe03ff476831c054b617d9e11d96d31f35d0d94d
Anshu Sinha <anshujuly2@gmail.com>
2023-09-06 21:45:32 +0530

    Add two approaches for insertion sort

--- Coding_Ninjas/insertion_sort.cpp
+++ Coding_Ninjas/insertion_sort.cpp
@@ -1,20 +1,51 @@
-// ITERATIVE APRROACH
-void bubbleSort(vector<int> &arr, int n) 
-{
-    //write your code here
-    for (int i = n; i > 1; i -= 1) {
-        int swapped = 0;
-        for (int j = 0; j < i-1; j += 1) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-                swapped = 1;
-            }
-        }
-        if (swapped == 0) {
-            break;
-        }
+#include<iostream>
+using namespace std;
+
+// // ITERATIVE APPROACH
+// void insertionSort(int arr[], int n) {
+//     for (int i = 1; i < n; i += 1) {
+//         for(int j = i; j > 0 && arr[j-1] > arr[j]; j -= 1) {
+//             int temp = arr[j];
+//             arr[j] = arr[j-1];
+//             arr[j-1] = temp;
+//         }
+//     }
+// }
+
+// RECURSIVE APPROACH
+void insertionSort(int arr[], int n, int i = 1) {
+    if (i == n) { // counter for i
+        return;
     }
+
+    for (int j = i; j > 0 && arr[j-1] > arr[j]; j -= 1) {
+        int temp = arr[j];
+        arr[j] = arr[j-1];
+        arr[j-1] = temp;
+    }
+
+    insertionSort(arr, n, i += 1);
 }
 
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i += 1) {
+        cin >> arr[i];
+    }
+
+    insertionSort(arr, n);
+
+    for (int i = 0; i < n; i += 1) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+} 
