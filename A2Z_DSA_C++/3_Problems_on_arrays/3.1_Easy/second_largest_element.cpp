6d0eb01e7dd0bfd4300056f8db1eca290a067384
Anshu Sinha <anshujuly2@gmail.com>
2023-09-07 16:38:32 +0530

    Add better approach to find the second largest element in an array

--- A2Z_DSA_C++/3_Problems_on_arrays/3.1_Easy/second_largest_element.cpp
+++ A2Z_DSA_C++/3_Problems_on_arrays/3.1_Easy/second_largest_element.cpp
@@ -1,34 +1,59 @@
 #include<iostream>
 using namespace std;
 
-// BRUTE APPROACH
-int partition(int arr[], int low, int high) {
-    int pivot = arr[low];
-    int i = low;
-    int j = high;
-    while (i < j) {
-        while (arr[i] <= pivot && i <= high) {
-            i += 1;
-        }
-        while (arr[j] > pivot && j >= low) {
-            j -= 1;
-        }
-        if (i < j) {
-            swap(arr[i], arr[j]);
-        }
-    }
-    swap(arr[low], arr[j]);
-    return j;
-}
+// // BRUTE APPROACH: time complexity - o(nlogn + n)
+// int partition(int arr[], int low, int high) {
+//     int pivot = arr[low];
+//     int i = low;
+//     int j = high;
+//     while (i < j) {
+//         while (arr[i] <= pivot && i <= high) {
+//             i += 1;
+//         }
+//         while (arr[j] > pivot && j >= low) {
+//             j -= 1;
+//         }
+//         if (i < j) {
+//             swap(arr[i], arr[j]);
+//         }
+//     }
+//     swap(arr[low], arr[j]);
+//     return j;
+// }
  
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int p_index = partition(arr, low, high);
-        quickSort(arr, low, p_index - 1);
-        quickSort(arr, p_index + 1, high);
-    }
-}
+// void quickSort(int arr[], int low, int high) {
+//     if (low < high) {
+//         int p_index = partition(arr, low, high);
+//         quickSort(arr, low, p_index - 1);
+//         quickSort(arr, p_index + 1, high);
+//     }
+// }
 
+// int main() {
+//     int n;
+//     cout << "Enter number of elements: ";
+//     cin >> n;
+
+//     int arr[n];
+
+//     cout << "Enter array elements: ";
+//     for (int i = 0; i < n; i += 1) {
+//         cin >> arr[i];
+//     }
+
+//     quickSort(arr, 0, n-1);
+
+//     for (int i = n - 2; i >= 0; i -= 1) {
+//         if (arr[i] != arr[n-1]) {
+//             cout << arr[i] << endl;
+//             break;
+//         }
+//     }
+
+//     return 0;
+// }
+
+// BETTER APPROACH: time complexity - o(n+n) = o(2n) = o(n)
 int main() {
     int n;
     cout << "Enter number of elements: ";
@@ -39,16 +64,24 @@ int main() {
     cout << "Enter array elements: ";
     for (int i = 0; i < n; i += 1) {
         cin >> arr[i];
-    }
+    }  
 
-    quickSort(arr, 0, n-1);
+    int largest = arr[0];
+    int second_largest = arr[0];
 
-    for (int i = n - 2; i >= 0; i -= 1) {
-        if (arr[i] != arr[n-1]) {
-            cout << arr[i] << endl;
-            break;
+    for (int i = 0; i < n; i += 1) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    } 
+
+    for (int i = 0; i < n; i += 1) {
+        if (arr[i] != largest && arr[i] > second_largest) {
+            second_largest = arr[i];
         }
     }
+
+    cout << second_largest << endl;
 
     return 0;
 }
