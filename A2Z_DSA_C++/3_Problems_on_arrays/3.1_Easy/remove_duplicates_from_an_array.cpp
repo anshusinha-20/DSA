75e36033095514e9320d94d81f75d90653dc7558
Anshu Sinha <anshujuly2@gmail.com>
2023-09-07 19:36:12 +0530

    Add optimal approach to remove duplicates from an array

--- A2Z_DSA_C++/3_Problems_on_arrays/3.1_Easy/remove_duplicates_from_an_array.cpp
+++ A2Z_DSA_C++/3_Problems_on_arrays/3.1_Easy/remove_duplicates_from_an_array.cpp
@@ -2,7 +2,34 @@
 #include<set>
 using namespace std;
 
-// BRUTE APPROACH: time complexity - o(nlogn + n), space complexity - o(n)
+// // BRUTE APPROACH: time complexity - o(nlogn + n), space complexity - o(n)
+// int main() {
+//     int n;
+//     cout << "Enter number of elements: ";
+//     cin >> n;
+
+//     int arr[n];
+//     cout << "Enter array elements: ";
+//     for (int i = 0; i < n; i += 1) {
+//         cin >> arr[i];
+//     }
+
+//     set<int> s;
+//     for (int i = 0; i < n; i += 1) {
+//         s.insert(arr[i]);
+//     }
+
+//     int num_el = 0;
+//     for (auto i : s) {
+//         num_el += 1;
+//     }
+
+//     cout << num_el << endl;
+
+//     return 0; 
+// }
+
+// OPTIMAL APPROACH: time complexity - o(n), space complexity - o(1)
 int main() {
     int n;
     cout << "Enter number of elements: ";
@@ -14,17 +41,15 @@ int main() {
         cin >> arr[i];
     }
 
-    set<int> s;
-    for (int i = 0; i < n; i += 1) {
-        s.insert(arr[i]);
+    int i = 0;
+    for (int j = 1; j < n; j += 1) {
+        if (arr[j] != arr[i]) {
+            arr[i + 1] = arr[j];
+            i += 1;
+        }
     }
 
-    int num_el = 0;
-    for (auto i : s) {
-        num_el += 1;
-    }
+    cout << i+1 << endl;
 
-    cout << num_el << endl;
-
-    return 0; 
+    return 0;
 }
