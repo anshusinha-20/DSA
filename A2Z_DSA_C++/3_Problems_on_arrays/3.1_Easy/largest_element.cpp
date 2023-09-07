39a11393e6862d2366a10ad05943d6495ea89ea1
Anshu Sinha <anshujuly2@gmail.com>
2023-09-07 16:10:15 +0530

    Add an optimal approach to find the largest element from an array

--- A2Z_DSA_C++/3_Problems_on_arrays/3.1_Easy/largest_element.cpp
+++ A2Z_DSA_C++/3_Problems_on_arrays/3.1_Easy/largest_element.cpp
@@ -1,37 +1,53 @@
 #include<iostream>
 using namespace std;
 
-// BRUTE
+// // BRUTE: time complexity - o(nlogn), space complexity - o(1)
+// int partitionArray(int arr[], int low, int high) {
+//     int pivot = arr[low];
+//     int i = low;
+//     int j = high;
+//     while (i < j) {
+//         while (arr[i] <= pivot && i <= high) {
+//             i += 1;
+//         }
+//         while (arr[j] > pivot && j >= low) {
+//             j -= 1;
+//         }
+//         if (i < j) {
+//             swap(arr[i], arr[j]);
+//         }
+//     }
+//     swap(arr[low], arr[j]);
+//     return j;
+// }
 
-int partitionArray(int arr[], int low, int high) {
-    int pivot = arr[low];
-    int i = low;
-    int j = high;
-    while (i < j) {
-        while (arr[i] <= pivot && i <= high) {
-            i += 1;
-        }
-        while (arr[j] > pivot && j >= low) {
-            j -= 1;
-        }
-        if (i < j) {
-            swap(arr[i], arr[j]);
-        }
-    }
-    swap(arr[low], arr[j]);
-    return j;
-}
+// void quickSort(int arr[], int low, int high) {  
+//     if (low < high) {
+//         int p_index = partitionArray(arr, low, high);
+//         quickSort(arr, low, p_index - 1);
+//         quickSort(arr, p_index + 1, high);
+//     }
+// }
 
-void quickSort(int arr[], int low, int high) {  
-    if (low < high) {
-        int p_index = partitionArray(arr, low, high);
-        quickSort(arr, low, p_index - 1);
-        quickSort(arr, p_index + 1, high);
-    }
-}
+// int main (){
+//     int n;
+//     cout << "Enter number of elements: ";
+//     cin >> n;
 
-int main (){
-    int n;
+//     int arr[n];
+
+//     cout << "Enter array elements: ";
+//     for (int i = 0; i < n; i += 1) {
+//         cin >> arr[i];
+//     }
+
+//     quickSort(arr, 0, n - 1);
+//     cout << arr[n-1] << endl;
+// }
+
+// OPTIMAL APPROACH: time complextiy: o(n)
+int main() {
+    int n; 
     cout << "Enter number of elements: ";
     cin >> n;
 
@@ -42,6 +58,15 @@ int main (){
         cin >> arr[i];
     }
 
-    quickSort(arr, 0, n - 1);
-    cout << arr[n-1] << endl;
+    int largest = arr[0];
+
+    for (int i = 0; i < n; i += 1) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    cout << largest << endl;
+
+    return 0;
 }
