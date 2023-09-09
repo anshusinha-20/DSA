b9b6c6236324f0043c419799398639e881deafe2
Anshu Sinha <anshujuly2@gmail.com>
2023-09-09 15:41:02 +0530

    Add a better to find the missing number in an array

--- A2Z_DSA_C++/3_Problems_on_arrays/3.1_Easy/missing_number.cpp
+++ A2Z_DSA_C++/3_Problems_on_arrays/3.1_Easy/missing_number.cpp
@@ -1,7 +1,35 @@
 #include<iostream>
 using namespace std;
 
-// BRUTE APPROACH: time complexity - o(n^2), space complexity - o(1)
+// // BRUTE APPROACH: time complexity - o(n^2), space complexity - o(1)
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n-1];
+//     cout << "Enter " << n-1 << " inclusive numbers between 1 and " << n << ": ";
+//     for (int i = 0; i < n - 1; i++) {
+//         cin >> arr[i];
+//     }
+
+//     for (int i = 1; i <= n; i++) {
+//         int flag = 0;
+//         for (int j = 0; j < n-1; j++) {
+//             if (arr[j] == i) {
+//                 flag = 1;
+//                 break;
+//             }
+//         }
+//         if (flag == 0) {
+//             cout << i << endl;
+//             break;
+//         }
+//     }
+
+//     return 0;
+// }
+
+// BETTER APPROACH
 int main() {
     int n;
     cin >> n;
@@ -12,15 +40,13 @@ int main() {
         cin >> arr[i];
     }
 
+    vector<int> hashh(n+1, 0);
+    for (int i = 0; i < n - 1; i += 1) {
+        hashh[arr[i]] += 1;
+    }
+
     for (int i = 1; i <= n; i++) {
-        int flag = 0;
-        for (int j = 0; j < n-1; j++) {
-            if (arr[j] == i) {
-                flag = 1;
-                break;
-            }
-        }
-        if (flag == 0) {
+        if(hashh[i] == 0) {
             cout << i << endl;
             break;
         }
