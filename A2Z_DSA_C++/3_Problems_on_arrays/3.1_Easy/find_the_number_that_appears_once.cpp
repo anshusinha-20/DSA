a4b0932bf463ecfa298c59a75a867ad3aabc9a1d
Anshu Sinha <anshujuly2@gmail.com>
2023-09-10 19:51:29 +0530

    Add a better approach to find the number that appears once in an array

--- A2Z_DSA_C++/3_Problems_on_arrays/3.1_Easy/find_the_number_that_appears_once.cpp
+++ A2Z_DSA_C++/3_Problems_on_arrays/3.1_Easy/find_the_number_that_appears_once.cpp
@@ -1,25 +1,51 @@
 #include<iostream>
+#include<map>
 using namespace std;
 
-// BRUTE APPROACH: time complexity - o(n^2), space complexity - o(1)
+// // BRUTE APPROACH: time complexity - o(n^2), space complexity - o(1)
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     for (int i = 0; i < n; i++) {
+//         int count = 0;
+//         for (int j = 0; j < n; j++) {
+//             if (arr[j] == arr[i]) {
+//                 count += 1;
+//             }
+//         }
+//         if (count == 1) {
+//             cout << arr[i] << endl;
+//             break;
+//         }
+//     }
+
+//     return 0;
+// }
+
+// BETTER APPRAOCH - time complexity - o(n), space complexity - o(n)
 int main() {
-    int n;
+    int n; // number of elements in the array
     cin >> n;
 
-    int arr[n];
+    int arr[n]; // filling array with the elements
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
+    map<int, int> m; // creating a map with elements as keys and their number of occurences as values
     for (int i = 0; i < n; i++) {
-        int count = 0;
-        for (int j = 0; j < n; j++) {
-            if (arr[j] == arr[i]) {
-                count += 1;
-            }
-        }
-        if (count == 1) {
-            cout << arr[i] << endl;
+        m[arr[i]] += 1;
+    }
+
+    for (auto i : m) {
+        if (i.second == 1) { // if value is 1
+            cout << i.first << endl; // print the key storing value 1
             break;
         }
     }
