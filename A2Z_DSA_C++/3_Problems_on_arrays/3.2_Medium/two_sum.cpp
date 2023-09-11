7801d2a06372311ec594f9ee5fcb43b7a2b088b6
Anshu Sinha <anshujuly2@gmail.com>
2023-09-11 14:46:49 +0530

    Add an optimal approach to find the two sum

--- A2Z_DSA_C++/3_Problems_on_arrays/3.2_Medium/two_sum.cpp
+++ A2Z_DSA_C++/3_Problems_on_arrays/3.2_Medium/two_sum.cpp
@@ -39,7 +39,70 @@ using namespace std;
 //     return 0;
 // }
 
-// BETTER APPROACH: time complexity - o(n), space complexity - o(n)
+// // BETTER APPROACH: time complexity - o(nlogn), space complexity - o(n)
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     int k;
+//     cin >> k;
+
+//     map<int, int> m;
+//     string ans = "no";
+//     for (int i = 0; i < n; i++) {
+//         m[arr[i]] = i;
+//         if (m.find(k-arr[i]) != m.end()) { // 14 - 6 = 8, if 8 exits in the map, return yes
+//             ans = "yes";
+//             break;
+//         }
+//     }
+
+//     cout << ans << endl;
+
+//     return 0;
+
+// }
+
+// OPTIMAL APPROACH
+int partitionArray(int input[], int start, int end) {
+	// Write your code here
+	int pivot = input[start];
+	int i = start;
+	int j = end;
+
+	while (i < j) {
+		while (input[i] <= pivot && i <= end) {
+			i += 1;
+		}
+		while (input[j] > pivot && j >= start) {
+			j -= 1;
+		}
+		if (i < j) {
+			swap(input[i], input[j]);
+		}
+	}
+	swap(input[start], input[j]);
+	return j;
+}
+
+void quickSort(int input[], int start, int end) {
+	/*
+		Don't write main().
+		Don't read input, it is passed as function argument.
+		Change in the given array itself.
+		Taking input and printing output is handled automatically.
+	*/
+	if (start < end) {
+		int p_index = partitionArray(input, start, end);
+		quickSort(input, start, p_index - 1);
+		quickSort(input, p_index + 1, end);
+	}
+}
 int main() {
     int n;
     cin >> n;
@@ -49,23 +112,29 @@ int main() {
         cin >> arr[i];
     }
 
+    quickSort(arr, 0, n-1);
+
     int k;
     cin >> k;
 
-    map<int, int> m;
     string ans = "no";
-    for (int i = 0; i < n; i++) {
-        m[arr[i]] = i;
-        if (m.find(k-arr[i]) != m.end()) { // 14 - 6 = 8, if 8 exits in the map, return yes
+
+    int i = 0;
+    int j = n-1;
+    while (i < j) {
+        if (arr[i] + arr[j] == k) {
             ans = "yes";
             break;
+        }
+        else if (arr[i] + arr[j] < k) {
+            i++;
+        }
+        else {
+            j--;
         }
     }
 
     cout << ans << endl;
 
     return 0;
-
 }
-
-// OPTIMAL APPROACH
