8c398c9de97365632dec0ae9997c2cfc3b3a0f39
Anshu Sinha <anshujuly2@gmail.com>
2023-09-11 10:41:05 +0530

    Add a brute and a better approach to find the maximum subarray length for the sum k

---
+++ A2Z_DSA_C++/3_Problems_on_arrays/3.1_Easy/longest_subarray_with_given_sum_k.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+// // BRUTE APPROACH: time complexity(n^3), space complexity - o(n)
+// int main() {
+//     int n; // number of elements in the array
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++) { // filling the array with the elements
+//         cin >> arr[i];
+//     }
+
+//     int k; // sum k
+//     cin >> k;
+
+//     int length = 0;
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i; j < n; j++) {
+//             int sum = 0;
+//             for (int l = i; l <= j; l++) {
+//                 sum += arr[l];
+//             }
+//             if (sum == k) {
+//                 length = max(length, j-i+1);
+//             }
+//         }
+//     }
+
+//     cout << length << endl;
+
+//     return 0;
+// }
+
+// BETTER APPROACH
+int main() {
+    int n; // number of elements in the array
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) { // filling the array with the elements
+        cin >> arr[i];
+    }
+
+    int k; // sum k
+    cin >> k;
+
+    int length = 0;
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            sum += arr[j];
+            if (sum == k) {
+                length = max(length, j-i+1);
+            }
+        }
+    }
+
+    cout << length << endl;
+
+    return 0;
+}
