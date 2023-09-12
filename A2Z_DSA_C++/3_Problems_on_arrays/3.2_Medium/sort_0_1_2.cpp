2e09a4074f5c2fd841438198807813b9e4e24253
Anshu Sinha <anshujuly2@gmail.com>
2023-09-12 11:25:06 +0530

    Add a brute approach to sort the array with 0, 1 and 2

--- A2Z_DSA_C++/3_Problems_on_arrays/3.2_Medium/sort_0_1_2.cpp
+++ A2Z_DSA_C++/3_Problems_on_arrays/3.2_Medium/sort_0_1_2.cpp
@@ -1,6 +1,54 @@
 #include<iostream>
 using namespace std;
 
+// BRUTE APPRAOCH: time complexity - o(nlogn), space complexity - o(1)                                     
+int partition(int arr[], int l, int r) { 
+    int pivot = arr[l];
+    int i = l;
+    int j = r;
+
+    while (i < j) {
+        while (arr[i] <= pivot && i <= r) {
+            i++;
+        }
+        while (arr[j] > pivot && j >= l) {
+            j--;
+        }
+        if (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[l];
+    arr[l] = arr[j];
+    arr[j] = temp;
+    return j;
+}
+
+void quickSort(int arr[], int l, int r) {
+    if (l < r) {
+        int p_index = partition(arr, l, r);
+        quickSort(arr, l, p_index - 1);
+        quickSort(arr, p_index + 1, r);
+    }
+}
+
 int main() {
-    
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) { 
+        cin >> arr[i];
+    }
+
+    quickSort(arr, 0, n-1);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
