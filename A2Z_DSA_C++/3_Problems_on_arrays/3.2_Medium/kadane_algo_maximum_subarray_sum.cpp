bef97aafcfd36423b658e0e6d3a6fc55cae211bb
Anshu Sinha <anshujuly2@gmail.com>
2023-09-13 17:43:43 +0530

    Add an optimal approach to find the max sum in a subarray

--- A2Z_DSA_C++/3_Problems_on_arrays/3.2_Medium/kadane_algo_maximum_subarray_sum.cpp
+++ A2Z_DSA_C++/3_Problems_on_arrays/3.2_Medium/kadane_algo_maximum_subarray_sum.cpp
@@ -1,7 +1,57 @@
 #include<iostream>
 using namespace std;
 
-// BRUTE APPROACH
+// // BRUTE APPROACH: time complexity - o(n^3), space complexity - o(1)
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     int maxi = 0;
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+//             int sum = 0;
+//             for (int k = i; k <= j; k++) {
+//                 sum += arr[k];
+//             }
+//             maxi = max(sum, maxi);
+//         }
+//     }
+
+//     cout << maxi << endl;
+
+//     return 0;
+// }
+
+// // BETTER APPROACH: time complexity - o(n^2), space complexity - o(1)
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     int maxi = 0;
+//     for (int i = 0; i < n; i++) {
+//         int sum = 0;
+//         for (int j = i; j < n; j++) {
+//             sum += arr[j];
+//             maxi = max(sum, maxi);
+//         }
+//     }
+
+//     cout << maxi << endl;
+
+//     return 0;
+// }
+
+// OPTIMAL APPROACH: time complexity - o(n), space complexity - o(1), kadane's algo
 int main() {
     int n;
     cin >> n;
@@ -11,18 +61,25 @@ int main() {
         cin >> arr[i];
     }
 
-    int maxi = 0;
+    int sum = 0;
+    int maxi = INT_MIN;
+
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            int sum = 0;
-            for (int k = i; k <= j; k++) {
-                sum += arr[k];
-            }
+        sum += arr[i];
+        if (sum < 0) {
+            sum = 0;
+        }
+        else {
             maxi = max(sum, maxi);
         }
     }
 
-    cout << maxi << endl;
+    if (maxi <= 0) {
+        cout << 0 << endl;
+    }
+    else {
+        cout << maxi << endl;
+    }
 
     return 0;
 }
