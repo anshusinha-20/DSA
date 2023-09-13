9510d8148ac43f82842c67f5ff79b67f93008a56
Anshu Sinha <anshujuly2@gmail.com>
2023-09-13 16:15:47 +0530

    Add an optimal approach to find the majority element existing more than n/2 times in an n sized array

--- A2Z_DSA_C++/3_Problems_on_arrays/3.2_Medium/majority_element_nby2_times.cpp
+++ A2Z_DSA_C++/3_Problems_on_arrays/3.2_Medium/majority_element_nby2_times.cpp
@@ -35,7 +35,37 @@ using namespace std;
 
 // }
 
-// BETTER APPROACH: time complexity - o(n), space complexity - o(n)
+// // BETTER APPROACH: time complexity - o(n), space complexity - o(n)
+// int main () {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     int half = n/2;
+
+//     map<int, int> m;
+//     for (int i = 0; i < n; i++) {
+//         m[arr[i]]++;
+//     }
+
+//     for (auto i : m) {
+//         if (i.second > half) {
+//             cout << i.first << endl;
+//             break;
+//         }
+//     }
+
+//     return 0;
+// }
+
+// OPTIMAL APPROACH: Moore's voting algo
+// 1. find the element which exists even after cancelling out all the elements with each other
+// 2. iterate through the array again to verify
+
 int main () {
     int n;
     cin >> n;
@@ -45,18 +75,33 @@ int main () {
         cin >> arr[i];
     }
 
+    int count1 = 0;
+
     int half = n/2;
 
-    map<int, int> m;
-    for (int i = 0; i < n; i++) {
-        m[arr[i]]++;
+    int el = arr[0];
+    for (int i = 0; i < n-1; i++) {
+        if (arr[i] == el) {
+            count1++;
+        }
+        else {
+            count1--;
+            if (count1 == 0) {
+                el = arr[i+1];
+            }
+        }
     }
 
-    for (auto i : m) {
-        if (i.second > half) {
-            cout << i.first << endl;
-            break;
+    int count2 = 0; 
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == el) {
+            count2++;
         }
+    }
+
+    if (count2 > half) {
+        cout << el << endl;
     }
 
     return 0;
