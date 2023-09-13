081568dd5c93688d8ef31738093533ead6548f10
Anshu Sinha <anshujuly2@gmail.com>
2023-09-13 15:50:22 +0530

    Add better approach to find the majority element existing more than n/2 times in an n sized array

--- A2Z_DSA_C++/3_Problems_on_arrays/3.2_Medium/majority_element_nby2_times.cpp
+++ A2Z_DSA_C++/3_Problems_on_arrays/3.2_Medium/majority_element_nby2_times.cpp
@@ -1,7 +1,41 @@
 #include<iostream>
+#include<map>
 using namespace std;
 
-// BRUTE APPROACH
+// // BRUTE APPROACH: time complexity - o(n^2), space complexity - o(1)
+// int main () {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     int half = n/2;
+
+//     int count = 0;
+
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+//             if (arr[i] == arr[j]) {
+//                 count++;
+//             }
+//         }
+//         if (count > half) {
+//             cout << arr[i] << endl;
+//             break;
+//         }
+//         else {
+//             count = 0;
+//         }
+//     }
+
+//     return 0;
+
+// }
+
+// BETTER APPROACH: time complexity - o(n), space complexity - o(n)
 int main () {
     int n;
     cin >> n;
@@ -13,23 +47,17 @@ int main () {
 
     int half = n/2;
 
-    int count = 0;
-
+    map<int, int> m;
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (arr[i] == arr[j]) {
-                count++;
-            }
-        }
-        if (count > half) {
-            cout << arr[i] << endl;
+        m[arr[i]]++;
+    }
+
+    for (auto i : m) {
+        if (i.second > half) {
+            cout << i.first << endl;
             break;
-        }
-        else {
-            count = 0;
         }
     }
 
     return 0;
-
 }
