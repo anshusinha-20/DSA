803c741028418f2d5251df3b75d04482d0521755
Anshu Sinha <anshujuly2@gmail.com>
2023-09-13 22:51:16 +0530

    Add a code on buy and sell stock

---
+++ Coding_Ninjas/but_and_sell_stock.cpp
@@ -0,0 +1,13 @@
+int bestTimeToBuyAndSellStock(vector<int>&prices) {
+    // Write your code here.
+    int mini = prices[0];
+    int profit = 0;
+    int n = prices.size();
+    for (int i = 1; i < n; i++) {
+        int cost = prices[i] - mini;
+        profit = max(profit, cost);
+        mini = min(prices[i], mini);
+    }
+
+    return profit;
+}
