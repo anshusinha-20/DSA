9ac05d53d0a25ded0c1bf6c273704b653e736349
Anshu Sinha <anshujuly2@gmail.com>
2023-09-13 22:53:54 +0530

    Add a code on buy and sell stock

---
+++ A2Z_DSA_C++/3_Problems_on_arrays/3.2_Medium/buy_and_sell_stock.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int mini = arr[0];
+    int profit = 0;
+    for (int i = 1; i < n; i++) {
+        int cost = arr[i] - mini;
+        profit = max(cost, profit);
+        mini = min(mini, arr[i]);
+    }
+
+    cout << profit << endl;
+
+    return 0;
+}
