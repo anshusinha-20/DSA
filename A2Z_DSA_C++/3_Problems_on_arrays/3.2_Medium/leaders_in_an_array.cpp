6fb3e533fb00183376f042253e2e74b9be989b79
Anshu Sinha <anshujuly2@gmail.com>
2023-09-17 23:44:49 +0530

    Add an optimal approach to find the leaders in an array

--- A2Z_DSA_C++/3_Problems_on_arrays/3.2_Medium/leaders_in_an_array.cpp
+++ A2Z_DSA_C++/3_Problems_on_arrays/3.2_Medium/leaders_in_an_array.cpp
@@ -2,7 +2,40 @@
 #include<vector>
 using namespace std;
 
-// BRUTE APPROACH: time complexity - o(n^2), space complexity - o(n)
+// // BRUTE APPROACH: time complexity - o(n^2), space complexity - o(n)
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     vector<int> result;
+//     bool leader;
+//     for (int i = 0; i < n; i++) {
+//         leader = true;
+//         for (int j = i+1; j < n; j++) {
+//             if (arr[j] >= arr[i]) {
+//                 leader = false;
+//                 break;
+//             }
+//         }
+//         if (leader == true) {
+//             result.emplace_back(arr[i]);
+//         }
+//     }
+
+//     for (int i = 0; i < result.size(); i++) {
+//         cout << result[i] << " ";
+//     }
+//     cout << endl;
+
+//     return 0;
+// }
+
+// OPTIMAL APPROACH: time complexity - o(n), space complexity - o(n)
 int main() {
     int n;
     cin >> n;
@@ -13,17 +46,11 @@ int main() {
     }
 
     vector<int> result;
-    bool leader;
-    for (int i = 0; i < n; i++) {
-        leader = true;
-        for (int j = i+1; j < n; j++) {
-            if (arr[j] > arr[i]) {
-                leader = false;
-                break;
-            }
-        }
-        if (leader == true) {
-            result.emplace_back(arr[i]);
+    int maxi = INT_MIN;
+    for (int i = n-1; i >= 0; i--) {
+        if (arr[i] > maxi) {
+            maxi = arr[i];
+            result.emplace_back(maxi);
         }
     }
 
