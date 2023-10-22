614f3d52983306a7991cb0af61a981fe1e126550
Anshu Sinha <anshujuly2@gmail.com>
2023-10-22 10:59:20 +0530

    modify

--- Introduction_to_algorithms/2_Getting_started/insertion_sort.cpp
+++ Introduction_to_algorithms/2_Getting_started/insertion_sort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void insertion_sort(int arr[], int n) {
-    for (int i = 2; i < n; i++) {
+    for (int i = 1; i < n; i++) {
         int key = arr[i];
         int j = i-1;
         while(j >= 0 && arr[j] > key) {
